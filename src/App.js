7d2d1b08b3423b6babfccf3b3a575be2daf4b94b
honuhonu830 <mizujun830@gmail.com>
2019-11-01 22:25:59 +0900

    mizu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./components/Home";
+import targetRegistry from "./components/targetRegistry";
+import goukakuki from "./components/goukakuki";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={targetRegistry} />
+        <Route exact path="/goukakuki" component={goukakuki} />
       </BrowserRouter>
     </div>
   );
