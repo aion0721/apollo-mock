46b585a7979d56e7082493676510cd689f8f4288
chiitamu <chiitamu0312@gmail.com>
2019-11-01 22:16:36 +0900

    mi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
+import profile from "./components/profile"
+import mokuhyoukannri from "./components/mokuhyoukannri"
+import jissekikannri from "./components/jissekikannri"
+import jissekisyousai from "./components/jissekisyousai"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={profile} />
+        <Route exact path="/mokuhyoukannri" component={mokuhyoukannri} />
+        <Route exact path="/jissekikannri" component={jissekikannri} />
+        <Route exact path="/jissekisyousai" component={jissekisyousai} />
       </BrowserRouter>
     </div>
   );
